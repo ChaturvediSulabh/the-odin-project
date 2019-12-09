c17e2d709a40f65f7e23c1fc116b8456c6f9385c
ChaturvediSulabh <sulabhchaturvedi08@gmail.com>
2019-12-09 15:55:01 +0000

    bubble sort implementation

---
+++ Advanced_Building_Blocks/Assignment-1/bubble_sort.rb
@@ -0,0 +1,29 @@
+class BubbleSort
+
+  def bubble_sort(arr)
+    sorted = true
+    while sorted
+      sorted = false
+      (arr.length - 1).times do |i|
+        (arr[i], arr[i + 1] = arr[i + 1], arr[i]; sorted = true) if arr[i] > arr[i + 1]
+      end
+    end
+    arr
+  end
+
+  def bubble_sort_by(arr)
+    i = 0
+    while i < arr.length
+      result = yield(arr[i], arr[i + 1])
+      arr[i], arr[i + 1] = arr[i + 1], arr[i] if result == -1
+      i += 1
+    end
+    arr
+  end
+
+end
+
+# run = BubbleSort.new
+# print run.bubble_sort([4, 3, 78, 2, 0, 2])
+# puts ""
+# print run.bubble_sort_by(['hi', 'hello', 'hey']) { |left, right| left <=> right }
